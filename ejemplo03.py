5b74455baeb91c5f0f4e6ad7e885f90c29e156d5
Jean quispe <jeanquispe05@gmail.com>
2021-09-13 20:26:47 -0500

    primer clase

---
+++ ejemplo03.py
@@ -0,0 +1,7 @@
+import math
+r = int(input("r1: "))
+print(math.pi*r**2)
+
+# r = int(input("Ingresar radio: "))
+# area= math.pi*math.pow(r,2)
+# print("La radio es: ", r)
---
+++ ejemplo04.py
@@ -0,0 +1,3 @@
+import math
+x=2*math.sqrt(31)+4*math.sqrt(31)
+print("El resultado es: ", x)
